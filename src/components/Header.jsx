75e853cfd06f389cc4d3a20be47297ccd566ba55
rory lalor <rorylalor83@gmail.com>
2025-06-19 22:23:57 +0700

    setup the basic base to continue on

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+function Header() {
+  return (
+    <header>
+      <h1><MenuBookIcon></MenuBookIcon> MenuMaestro</h1><h6 >By Chefs, for Chefs</h6>
+    </header>
+  );
+}
+
+export default Header;
